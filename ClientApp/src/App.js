b24b39cbf82f2ee8266b1f46f491006193ad5b60
nvdhau <nvdhau@gmail.com>
2020-04-23 00:41:20 -0700

    added create trip form

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,9 +4,10 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-import { Trips } from './components/Trip/Trips';
 
 import './custom.css'
+import { Trips } from './components/Trip/Trips';
+import { Create } from './components/Trip/Create';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -15,8 +16,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/create' component={Create} />
         <Route exact path='/trips' component={Trips} />
       </Layout>
     );
